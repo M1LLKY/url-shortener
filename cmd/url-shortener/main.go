3af427e3ca799f30e40cf0f9cdebe71a7a42a361
m1llky <m1llky@bk.ru>
2025-06-15 15:21:55 +0300

    v1.0

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,40 +4,41 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
+	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/redirect"
+	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
 	"url-shortener/internal/lib/logger/handlers/slogpretty"
 	"url-shortener/internal/lib/logger/sl"
 	"url-shortener/internal/storage/postgres"
 )
 
-const (
+const ( // Объявляем константы для смены окружения
 	envLocal = "local"
 	envDev   = "dev"
 	envProd  = "prod"
 )
 
 func main() {
-	// TODO: init config: cleanenv
+	// TODO: init config: cleanenv | инициализируем конфиг
 	cfg := config.MustLoad()
 
-	// TODO: init logger: slog
+	// TODO: init logger: slog | инициализируем логгер
 	log := setupLogger(cfg.Env)
 
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
 
-	// TODO: init db: pgx (postgreSQL)
+	// TODO: init db: pgx (postgreSQL) | инициализируем подключение к БД
 	storage, err := postgres.New(cfg.StoragePath)
 	if err != nil {
 		log.Error("failed to initialize storage", sl.Err(err))
 		os.Exit(1)
 	}
 
-	_ = storage
-
-	// TODO: init router : chi, "chi render"
+	// TODO: init router : chi, "chi render" | инициализируем роутер и
 	router := chi.NewRouter()
 
 	// middleware
@@ -47,7 +48,32 @@ func main() {
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.URLFormat)
 
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(log, storage))
+	})
+
+	router.Get("/{alias}", redirect.New(log, storage))
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.Timeout,
+	}
+
 	// TODO: run server:
+	if err := srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
